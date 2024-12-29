3641c26a8e6cc3427e2b78e8500672a1e4045dde
Charis Cat <65819058+ChildOfAnAndroid@users.noreply.github.com>
2024-12-29 00:07:51 +0000

    global variables etc

--- Main.py
+++ Main.py
@@ -11,26 +11,128 @@ from scipy.ndimage import gaussian_filter
 from matplotlib.colors import LinearSegmentedColormap
 from config import *
 
-# death statistics
-deathSquishCounter = 0
-deathAgeCounter = 0
-deathStarvationCounter = 0
-deathEscapeCounter = 0
+class Stats:
+    def __init__(self):
+        # Population
+        self.cellCounter = 0
 
-# birth statistics
-babyCounter = 0
+        # death statistics
+        self.cellDeathCounter = {}
+        self.cellDeathEscapeCounter = 0
 
-# TURN STATISTICS
-counter = 0
-babysThisTurn = babyCounter
-deathsThisTurn = deathSquishCounter + deathAgeCounter + deathStarvationCounter
-deathEscapesThisTurn = deathEscapeCounter
-pushedCellCounter = 0
-aliveCount = 0
+        # birth statistics
+        self.cellBabyCounter = 0
+        self.cellBabysFailedCounter = 0
+
+        # Movement statistics
+        self.cellMovedCounter = 0
+        self.cellPushedCounter = 0
+
+        # TURN STATISTICS
+        self.cellBabysThisTurn = 0
+        self.cellBabysFailedThisTurn = 0
+        self.cellDeathsThisTurn = {}
+        self.cellDeathEscapesThisTurn = 0
+        self.cellMovedThisTurn = 0
+        self.cellPushedThisTurn = 0
+        self.cellAliveCount = 0
+
+    def beginTurn(self):
+        self.cellBabysThisTurn = 0
+        self.cellDeathsThisTurn = {}
+        self.cellDeathEscapesThisTurn = 0
+        self.cellBabysFailedThisTurn = 0
+        self.cellAliveCount = 0
+        self.cellPushedThisTurn = 0
+        self.cellMovedThisTurn = 0
+
+    def endTurn(self):
+        print(f"Turn Summary: There are currently {self.cellAliveCount} living cells. There were {self.cellBabysThisTurn} babies born, {self.getDeathsThisTurn()} cells died, and {self.cellDeathEscapesThisTurn} cells evaded death! ")
+        print(self)
+
+    def __str__(self):
+        return f"""
+Total Cell Count: {self.cellCounter+1}
+
+# Death Statistics
+Total Cell Death: {self.getTotalDeath()}
+By Reason:
+{"\n".join([f"{x}: {self.cellDeathCounter[x]}" for x in self.cellDeathCounter])}
+Total Cell Escapes: {self.cellDeathEscapeCounter}
+
+# Birth Statistics
+Total Cell Babies: {self.cellBabyCounter}
+Total Cell Skipped Babies: {self.cellBabysFailedCounter}
+
+# Movement Statistics
+Total Cell Movements: {self.cellMovedCounter}
+Total Cell Pushed: {self.cellPushedCounter}
+
+# Turn Statistics
+Cell Babies: {self.cellBabysThisTurn}
+Cell Skipped Babies: {self.cellBabysFailedThisTurn}
+Cell Death: {self.getDeathsThisTurn()}
+{"\n".join([f"{x}: {self.cellDeathsThisTurn[x]}" for x in self.cellDeathsThisTurn])}
+Cell Escapes: {self.cellDeathEscapesThisTurn}
+Cell Movements: {self.cellMovedThisTurn}
+Cell Pushes: {self.cellPushedThisTurn}
+Cells Alive: {self.cellAliveCount}
+"""
+
+    def addCellBaby(self):
+        self.cellBabyCounter += 1
+        self.cellBabysThisTurn += 1
+    
+    def addCellBabyFailed(self):
+        self.cellBabysFailedCounter += 1
+        self.cellBabysFailedThisTurn += 1
+    
+    def addCellDeath(self, reason):
+        if reason in self.cellDeathCounter:
+            self.cellDeathCounter[reason] += 1
+        else:
+            self.cellDeathCounter[reason] = 1
+        
+        if reason in self.cellDeathsThisTurn:
+            self.cellDeathsThisTurn[reason] += 1
+        else:
+            self.cellDeathsThisTurn[reason] = 1
+
+    def addCellDeathEscape(self):
+        self.cellDeathEscapeCounter += 1
+        self.cellDeathEscapesThisTurn += 1
+
+    def addCellPush(self):
+        self.cellPushedCounter += 1
+        self.cellPushedThisTurn += 1
+    
+    def addCellAlive(self):
+        self.cellAliveCount += 1
+
+    def addCellMove(self):
+        self.cellMovedCounter += 1
+        self.cellMovedThisTurn += 1
+
+    def getTotalDeath(self):
+        total = 0
+        for reason in self.cellDeathCounter:
+            total += self.cellDeathCounter[reason]
+        return total
+
+    def getDeathsThisTurn(self):
+        sum = 0
+        for reason in self.cellDeathsThisTurn:
+            sum += self.cellDeathsThisTurn[reason]
+        return sum
+
+    def getCellNextID(self):
+        ret = self.cellCounter
+        self.cellCounter += 1
+        return ret
 
 class Cell:
-    def __init__(self, x, y, id, organism=None):
-        self.id = id
+    def __init__(self, x, y, stats, organism=None):
+        self.id = stats.getCellNextID()
         self.step_count = 0
         self.x = x
         self.y = y
@@ -46,7 +148,7 @@ class Cell:
         self.phase_transition() # Call to set cell state & color
         self.light_emission = CELL_BASE_LIGHT_EMISSION_MIN  # Amount of light emitted (e.g., by plasma or bioluminescence)
         self.light_absorption = random.uniform(CELL_BASE_LIGHT_ABSORPTION_MIN, CELL_BASE_LIGHT_ABSORPTION_MAX)  # Ability to absorb light
-        self.attractiveness = CELL_BASE_ATTRACTIVENESS
+        self.attractiveness = CELL_BASE_ATTRACTIVENESS_MIN
         self.genome = {
             'growth_rate': self.growth_rate,
             'resilience': self.resilience,
@@ -55,9 +157,11 @@ class Cell:
             'role': self.role
         }
         self.organism = organism  # Tracks which organism this cell belongs to
+        self.stats = stats
+        
 
 
-    def move_or_squish(self, moving, direction, grid, inertGrid, deathCounter):
+    def move_or_squish(self, moving, direction, grid, inertGrid):
         dx, dy = direction
         new_x = (self.x + dx) % grid.shape[0]
         new_y = (self.y + dy) % grid.shape[1]
@@ -68,7 +172,8 @@ class Cell:
             self.y = new_y
             grid[self.x, self.y] = self
             print(f"Escaped a death squish!")
-            deathEscapeCounter += 1
+            self.stats.addCellDeathEscape()
+            self.stats.addCellMove()
             if not self.alive: # if cell is already inert and needs to move, update inertGrid
                 inertGrid[self.x, self.y] += 300
             return True
@@ -82,7 +187,7 @@ class Cell:
                     self.alive = False
                     grid[self.x, self.y] = None
                     print(f"Died from cuddles. Energy: {self.energy}, lost {(1 / self.resilience) * self.speed} this turn {self}")
-                    deathSquishCounter += 1
+                    self.stats.addCellDeath(CELL_DEATH_REASON_SQUISH)
                     inertGrid[self.x, self.y] += CELL_DEATH_RELEASE_INERT
                     return False
                 else:
@@ -92,7 +197,8 @@ class Cell:
                     self.x, self.y = (new_x, new_y)
                     grid[self.x, self.y] = self
                     print(f"The Vengabus is Evolving O.o")
-                    pushedCellCounter += 1
+                    self.stats.addCellPush()
+                    self.stats.addCellMove()
                     if not self.alive:
                         inertGrid[self.x, self.y] += CELL_DEATH_RELEASE_INERT
                     
@@ -141,6 +247,7 @@ class Cell:
             grid[self.x, self.y] = None
             self.x, self.y = new_x, new_y
             grid[self.x, self.y] = self
+            self.stats.addCellMove()
         else:
             if (dx, dy) == (0, 1) and blockCounter < 4:
                 dx, dy = (1, 0)
@@ -149,6 +256,7 @@ class Cell:
                 grid[self.x, self.y] = None
                 self.x, self.y = (self.x + dx) % grid.shape[0], (self.y + dy) % grid.shape[1]
                 grid[self.x, self.y] = self
+                self.stats.addCellMove()
             if (dx, dy) == (1, 0) and blockCounter < 4:
                 dx, dy = (0, -1)
                 # print(f"Blocked, attempting to move to {new_x, new_y}")
@@ -156,6 +264,7 @@ class Cell:
                 grid[self.x, self.y] = None
                 self.x, self.y = (self.x + dx) % grid.shape[0], (self.y + dy) % grid.shape[1]
                 grid[self.x, self.y] = self
+                self.stats.addCellMove()
             if (dx, dy) == (0, -1) and blockCounter < 4:
                 dx, dy = (-1, 0)
                 # print(f"Blocked, attempting to move to {new_x, new_y}")
@@ -163,6 +272,7 @@ class Cell:
                 grid[self.x, self.y] = None
                 self.x, self.y = (self.x + dx) % grid.shape[0], (self.y + dy) % grid.shape[1]
                 grid[self.x, self.y] = self
+                self.stats.addCellMove()
             if (dx, dy) == (-1, 0) and blockCounter < 4:
                 dx, dy = (0, 1)
                 # print(f"Blocked, attempting to move to {new_x, new_y}")
@@ -170,6 +280,7 @@ class Cell:
                 grid[self.x, self.y] = None
                 self.x, self.y = (self.x + dx) % grid.shape[0], (self.y + dy) % grid.shape[1]
                 grid[self.x, self.y] = self
+                self.stats.addCellMove()
             else:
                 if self.x != new_x or self.y != new_y:
                     print(f"Failed moving {self.id} from ({self.x}, {self.y}) to ({new_x}, {new_y})")
@@ -235,48 +346,49 @@ class Cell:
             
     # State of the cell: solid, liquid, gas, plasma, inert
     def phase_transition(self):
-        if self.energy > PLASMA_ENERGY:
+        if self.energy > CELL_STATE_PLASMA_ENERGY:
             self.state = "plasma"
             self.hue = random.uniform(CELL_STATE_PLASMA_COLOR_MIN, CELL_STATE_PLASMA_COLOR_MAX)
-        elif GAS_ENERGY < self.energy <= PLASMA_ENERGY:
+        elif CELL_STATE_GAS_ENERGY < self.energy <= CELL_STATE_PLASMA_ENERGY:
             self.state = "gas"
             self.hue = random.uniform(CELL_STATE_GAS_COLOR_MIN, CELL_STATE_GAS_COLOR_MAX)
-        elif LIQUID_ENERGY < self.energy <= GAS_ENERGY:
+        elif CELL_STATE_LIQUID_ENERGY < self.energy <= CELL_STATE_GAS_ENERGY:
             self.state = "liquid"
             self.hue = random.uniform(CELL_STATE_LIQUID_COLOR_MIN, CELL_STATE_LIQUID_COLOR_MAX)
-        elif SOLID_ENERGY < self.energy <= LIQUID_ENERGY:
+        elif CELL_STATE_SOLID_ENERGY < self.energy <= CELL_STATE_LIQUID_ENERGY:
             self.state = "solid"
             self.hue = random.uniform(CELL_STATE_SOLID_COLOR_MIN, CELL_STATE_SOLID_COLOR_MAX)
-        elif INERT_ENERGY < self.energy <= SOLID_ENERGY:
+        elif CELL_STATE_INERT_ENERGY < self.energy <= CELL_STATE_SOLID_ENERGY:
             self.state = "inert"
             self.hue = self.hue if hasattr(self, "hue") else random.uniform(CELL_STATE_INERT_COLOR_MIN, CELL_STATE_INERT_COLOR_MAX)
 
-    def reproduce(self, grid, lightGrid, counter):
-        if not self.alive or (self.age > CELL_FERTILITY_MAX_AGE) or (self.age < CELL_FERTILITY_MIN_AGE):
-            return
+    def reproduce(self, grid, lightGrid):
+        if not self.alive or (self.age > CELL_FERTILITY_AGE_MAX) or (self.age < CELL_FERTILITY_AGE_MIN):
+            return False
         # reproducing a cell inside an organism (will be done in organism)
         #if self.organism is not None:
         #    return
         # Generate a baby cell if enough energy
-        if random.random() < CELL_FERTILITY_CHANCE or self.attractiveness > 9 or self.energy > CELL_FERTILITY_ENERGY:
+        if random.random() < CELL_FERTILITY_CHANCE_MIN or self.attractiveness > 9 or self.energy > CELL_FERTILITY_ENERGY_MIN:
             x, y = (cell.x + random.choice([-1, 1])) % grid.shape[0], (cell.y + random.choice([-1, 1])) % grid.shape[1]
             if grid[x, y] == 0 or grid[x, y] is None:  # Empty spot
-                babyCounter += 1
-                counter += 1
                 self.energy = (self.energy/CELL_REPRODUCTION_SUCCESS_COST)
-                baby_cell = Cell(x, y, counter, organism=None)
+                baby_cell = Cell(x, y, self.stats, organism=None)
                 baby_cell.growth_rate = max(0.5, min(2.0, cell.growth_rate + random.uniform(CELL_BABY_MUTATION_GROWTH_MIN, CELL_BABY_MUTATION_GROWTH_MAX)))
                 baby_cell.resilience = max(0.5, min(2.0, cell.resilience + random.uniform(CELL_BABY_MUTATION_RESILIENCE_MIN, CELL_BABY_MUTATION_RESILIENCE_MAX)))
                 baby_cell.perception_strength = max(0.1, min(1.0, cell.perception_strength + random.uniform(CELL_BABY_MUTATION_PERCEPTION_MIN, CELL_BABY_MUTATION_PERCEPTION_MAX)))
-                baby_cell.speed = max(0.5, min(2.0, cell.speed + random.uniform(CELL_BABY_MUTATION_SPEED_MIN, CELL_BABY_MUTATION_SPEED_MIX)))
+                baby_cell.speed = max(0.5, min(2.0, cell.speed + random.uniform(CELL_BABY_MUTATION_SPEED_MIN, CELL_BABY_MUTATION_SPEED_MAX)))
                 baby_cell.role = random.choice(CELL_ROLES)
                 grid[x, y] = baby_cell
                 # print("UNEBEBEEEEEEEEEEEEEEEEE!!!!!!!!!!!!!!!!!!1!!!!!!!!!!!!!1!!!")
-                return babyCounter, counter
+                self.stats.addCellBaby()
+                return True
             else:
                 self.energy = (self.energy/CELL_REPRODUCTION_FAILURE_COST)
                 # print("Tried to UNEBEBEBEBEBEBEBEE BUT NO SPACE LEFT")
-                return babyCounter, counter
+                self.stats.addCellBabyFailed()
+                return False
+        return False
 
     def decay(self, lightGrid, inertGrid):
         if not self.alive:
@@ -288,12 +400,14 @@ class Cell:
         if self.energy <= 0 or self.age > (self.resilience * random.uniform(CELL_DEATH_AGE_MIN, CELL_DEATH_AGE_MAX)):  # Death by starvation or old age
             self.alive = False
             print(f"Died from state {self.state} Energy: {self.energy}, lost {(1 / self.resilience) * self.speed} this turn")
-            deathAgeCounter += 1
-            deathStarvationCounter += 1
+            if self.energy >= 0:
+                self.stats.addCellDeath(CELL_DEATH_REASON_AGE)
+            else:
+                self.stats.addCellDeath(CELL_DEATH_REASON_STARVATION)
             self.energy = 0
             self.state = "inert"
-            lightGrid[self.x, self.y] += CELL_DEATH_LIGHT_RELEASE  # Dead cells release light for some reason
-            inertGrid[self.x, self.y] += CELL_DEATH_INERT_RELEASE # Drop inert resources onto inert grid
+            lightGrid[self.x, self.y] += CELL_DEATH_RELEASE_LIGHT  # Dead cells release light for some reason
+            inertGrid[self.x, self.y] += CELL_DEATH_RELEASE_INERT # Drop inert resources onto inert grid
 
 class Organism:
     def __init__(self, organism_id):
@@ -336,7 +450,7 @@ next_organism_id = 1
 
 # Create grid and environment
 grid = np.zeros((GRID_SIZE, GRID_SIZE), dtype=object)  # Allow tracking cell states
-environment = np.random.random((GRID_SIZE, GRID_SIZE)) * 5  # Light levels
+lightGrid = np.random.random((GRID_SIZE, GRID_SIZE)) * 5  # Light levels
 waifuGrid = np.zeros((GRID_SIZE, GRID_SIZE))
 inertGrid = np.zeros((GRID_SIZE, GRID_SIZE)) # Fully decayed inerts
 
@@ -345,14 +459,14 @@ def enrich_environment(lightGrid, waifuGrid, inertGrid):
     # Brighten some random areas
     for _ in range(ENVIRONMENT_LIGHT_ENRICHMENT_SOURCE_NUM):  # Number of light sources
         x, y = random.randint(0, GRID_SIZE - 1), random.randint(0, GRID_SIZE - 1)
-        lightGrid[x, y] = min(lightGrid[x, y] + ENVIRONMENT_LIGHT_ENRICHMENT, ENVIRONMENT_LIGHT_MAX_CLIP)  # Cap at max brightness
-    lightGrid -= ENVIRONMENT_GRID_DECAY_RATE
-    lightGrid = np.clip(lightGrid, ENVIRONMENT_LIGHT_MIN_CLIP, ENVIRONMENT_LIGHT_MAX_CLIP)
+        lightGrid[x, y] = min(lightGrid[x, y] + ENVIRONMENT_LIGHT_ENRICHMENT, ENVIRONMENT_LIGHT_CLIP_MAX)  # Cap at max brightness
+    lightGrid -= LIGHT_GRID_DECAY_RATE
+    lightGrid = np.clip(lightGrid, ENVIRONMENT_LIGHT_CLIP_MIN, ENVIRONMENT_LIGHT_CLIP_MAX)
     waifuGrid -= ATTRACTIVENESS_GRID_DECAY_RATE
     inertGrid -= INERT_GRID_DECAY_RATE
-    #print(environment)
-    #print(np.min(environment))
-    #print(np.max(environment))
+    #print(lightGrid)
+    #print(np.min(lightGrid))
+    #print(np.max(lightGrid))
     #lightGrid = gaussian_filter(lightGrid, sigma=1)  # sigma = more/less blur
     #print(lightGrid)
 
@@ -365,29 +479,31 @@ def stir_environment(grid):
             dx, dy = random.choice([(0, 1), (1, 0), (0, -1), (-1, 0)])
             new_x = (x + dx) % GRID_SIZE
             new_y = (y + dy) % GRID_SIZE
-            if grid[new_x, new_y] == 0:
+            if grid[new_x, new_y] is None:
+                grid[new_x, new_y].stats.addCellMove()
                 grid[new_x, new_y] = grid[x, y]
-                grid[x, y] = 0
+                grid[x, y] = None
 
-def on_click(counter, event):
+
+def on_click(stats, event):
     # Handles mouse clicks to place cells
     if event.xdata is None or event.ydata is None:
         return
     x, y = int(event.ydata), int(event.xdata)
     if grid[x, y] == 0 or grid[x, y] is None:
-        new_cell = Cell(x, y, counter, organism=None)
+        new_cell = Cell(x, y, stats, organism=None)
         counter += 1
         new_cell.role = random.choice(CELL_ROLES)
         grid[x, y] = new_cell
         print(f"Placed a {new_cell.role} cell at ({x}, {y})")
 
 cells = []
+stats = Stats()
 for _ in range(CELL_BASE_COUNT):
     x, y = random.randint(0, GRID_SIZE - 1), random.randint(0, GRID_SIZE - 1)
     organism = Organism(next_organism_id)
     next_organism_id += 1
-    new_cell = Cell(x, y, counter, organism=organism)
-    counter += 1
+    new_cell = Cell(x, y, stats, organism=organism)
     new_cell.role = random.choice(CELL_ROLES)
     #new_cell.state = cell.state if random.random() < .001 else False #random.choice([True, False])
     cells.append(new_cell)
@@ -419,10 +535,11 @@ purple_yellow_colormap = LinearSegmentedColormap.from_list("purple_yellow", colo
 # Visualization and Interaction
 plt.ion()
 fig, ax = plt.subplots()
-cid = fig.canvas.mpl_connect('button_press_event', partial(on_click, counter))
+cid = fig.canvas.mpl_connect('button_press_event', partial(on_click, stats))
 top_energy = VISUALISATION_BASE_ENERGY_TOP_RECORD
 for step in range(NUM_STEPS):
     print(f"Step {step} start")
+    stats.beginTurn()
     ax.clear()
     #ax.imshow(gridSize, alpha=0.5)
     ax.imshow(inertGrid, cmap=terrain_colormap, alpha=INERT_GRID_TRANSPARENCY)
@@ -439,7 +556,7 @@ for step in range(NUM_STEPS):
             cell = grid[x,y]
             if isinstance(cell, Cell): #and cell.alive:
                 if cell.alive:
-                    aliveCount += 1
+                    stats.addCellAlive()
                     try:
                         ax.add_patch(plt.Rectangle((cell.y - 0.5, cell.x - 0.5), 1, 1, color=cell.get_color(top_energy), alpha=cell.get_alpha()))
                     except Exception as e:
@@ -472,10 +589,9 @@ for step in range(NUM_STEPS):
         full_key.append(patch)
     ax.legend(handles=full_key)
 
-    ax.set_title(f"Step {step + 1} ({aliveCount})")
-    print(f"Done plotting, {aliveCount} cells are alive and displayed. Starting env changes")
+    ax.set_title(f"Step {step + 1} ({stats.cellAliveCount})")
+    print(f"Done plotting, {stats.cellAliveCount} cells are alive and displayed. Starting env changes")
     plt.pause(0.1)
-    plt.show(block=True)  # Keeps the figure window open until you close it manually
 
     enrich_environment(lightGrid, waifuGrid, inertGrid)  # Replenish nutrients dynamically
 
@@ -491,14 +607,16 @@ for step in range(NUM_STEPS):
                     cell.move(grid, lightGrid, inertGrid)
                     cell.absorb_nutrients(lightGrid)
                     cell.phase_transition()
-                    cell.reproduce(grid, lightGrid, counter)
+                    cell.reproduce(grid, lightGrid)
                     cell.decay(lightGrid, inertGrid)
                     cell.waifuSignal(waifuGrid)
                     if cell.energy > top_energy:
                         top_energy = cell.energy
     print("Done with env")
     
-print(f"Turn Summary: There are currently {aliveCount} living cells. There were {babysThisTurn} babies born, {deathsThisTurn} cells died, and {deathEscapesThisTurn} cells evaded death! ")
+    stats.endTurn()
+
+print(stats)
 
 """
     new_organisms = []
